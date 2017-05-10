a97f226dd89c20617fb60a665d1443c89ec76047
evtuhovaleksander <evtuhov.aleksander@gmail.com>
2017-05-10 13:13:53 +0300

    shablon of bash file

---
+++ instalation.sh
@@ -0,0 +1,12 @@
+
+#apt-get install python3
+if[ -d "test" ]; then
+    #remove dir
+fi
+
+#git clone command
+#cd git dir
+
+#install from requerements
+
+#python3 manage.py runserver localhost:8000
